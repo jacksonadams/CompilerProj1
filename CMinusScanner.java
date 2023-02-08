ecdeb5d94729176cfa3e56dac7f1db6e4f510f2b
Jackson Adams <102924878+jacksonadams@users.noreply.github.com>
2023-02-08 14:45:39 -0500

    Add files via upload

---
+++ CMinusScanner.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.compilerproj1;
+
+/**
+ *
+ * @author jacksonadams
+ */
+public class CMinusScanner implements Scanner {
+    
+    private BufferedReader inFile;
+    private Token nextToken;
+    
+    public CMinusScanner (BufferedReader file) {
+        inFile = file;
+        nextToken = scanToken();
+    }
+    
+    public Token getNextToken () {
+        Token returnToken = nextToken;
+        if(nextToken.getType() != Token.TokenType.EOF_TOKEN){
+            nextToken = scanToken();
+        }
+        return returnToken;
+    }
+    public Token viewNextToken(){
+        return nextToken;
+    }
+}
